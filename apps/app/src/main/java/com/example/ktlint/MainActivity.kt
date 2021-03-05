66638d861bbffbc38e9811bef88973a6f0618be6
gustavoterras <gustavo.terras@neon.com.br>
2021-03-05 13:58:45 -0300

    teste

--- apps/app/src/main/java/com/example/ktlint/MainActivity.kt
+++ apps/app/src/main/java/com/example/ktlint/MainActivity.kt
@@ -11,7 +11,11 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
+
         setContentView(R.layout.activity_main)
+
 
 
 
--- apps/baa/src/main/java/com/example/baa/MainActivity.kt
+++ apps/baa/src/main/java/com/example/baa/MainActivity.kt
@@ -10,15 +10,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
-
-        
         setContentView(R.layout.activity_main)
-
-
-
-
 
 
 
