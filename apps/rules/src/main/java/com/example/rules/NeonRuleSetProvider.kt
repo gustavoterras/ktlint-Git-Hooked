a1990d0c2acdcce5da2e0666c968d5627143c40a
gustavoterras <gustavo.terras@neon.com.br>
2021-03-05 13:46:03 -0300

    teste

---
+++ apps/rules/src/main/java/com/example/rules/NeonRuleSetProvider.kt
@@ -0,0 +1,12 @@
+package com.example.rules
+
+import com.pinterest.ktlint.core.RuleSet
+import com.pinterest.ktlint.core.RuleSetProvider
+
+class NeonRuleSetProvider : RuleSetProvider {
+    override fun get(): RuleSet = RuleSet(
+        "neon-style-rule",
+        OneLineFunctionRule(),
+        NoVarRule()
+    )
+}
--- rules/src/main/java/com/example/rules/CustomRuleSetProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.example.rules
-
-import com.pinterest.ktlint.core.RuleSet
-import com.pinterest.ktlint.core.RuleSetProvider
-
-class CustomRuleSetProvider : RuleSetProvider {
-    override fun get(): RuleSet = RuleSet("my-custom-rule", OneLineFunctionRule(), NoVarRule())
-}
